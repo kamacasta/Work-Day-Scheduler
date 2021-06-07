939784b0c487dc72012e92f590bca170964bf048
kama casta <kamacasta983@gmail.com>
2021-06-06 18:35:14 -0700

    html fixed, added array and time display to jumbotron in JS

---
+++ script.js
@@ -0,0 +1,38 @@
+// universal variables 
+const workDayArr = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm'];
+
+// display time in jumbotron using moment.js
+var displayTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+$(".jumbotron").append(displayTime);
+
+
+// for loop created to 
+for (var i = 0; i < workDayArr.length; i++) {
+
+    // local variable containing the workDayArr (universal varaible)
+    var time = workDayArr[i];
+
+    
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// function for save button
+
+
+
+// function to update .aa and .remove class
+
+
+//load, date and call data from local storage
